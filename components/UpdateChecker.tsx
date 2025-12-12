902a3815ab5cd7b2876c15718f907f0c9858b7ab
Víctor Castillo <castillovictor2461@gmail.com>
2025-12-12 13:49:08 -0400

    versiones publicas

--- components/UpdateChecker.tsx
+++ components/UpdateChecker.tsx
@@ -4,7 +4,7 @@ import { ConfirmationModal } from './ui' // Tu modal existente
 import { Download } from 'lucide-react'
 
 // Esta versión debe coincidir con la que pongas en tu package.json
-const CURRENT_APP_VERSION = '1.0.0'
+const CURRENT_APP_VERSION = '1.1.0'
 
 export const UpdateChecker = () => {
     const [showUpdate, setShowUpdate] = useState(false)
