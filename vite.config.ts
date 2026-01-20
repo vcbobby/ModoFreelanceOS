60dab63018a4a2559d8eb5270d88806a6eb68ba9
Víctor Castillo <castillovictor2461@gmail.com>
2026-01-19 22:44:03 -0400

    correccion en vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '')
+
     return {
-        base: './',
+        base: '/', // 🔴 CLAVE ABSOLUTA
         server: {
             port: 3000,
             host: '0.0.0.0',
         },
         plugins: [react()],
-
         define: {
             'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
             'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
