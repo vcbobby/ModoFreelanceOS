3cd50a2433fa90ccf80f39f83596720d293cc4ec
Víctor Castillo <castillovictor2461@gmail.com>
2026-01-26 05:34:36 -0400

    modales

--- App.tsx
+++ App.tsx
@@ -1097,8 +1097,8 @@ const AppContent = () => {
                 </div>
             </aside>
 
-            <main className="flex-1 overflow-auto pt-16 md:pt-0 overscroll-none transform-gpu">
-                <div className="max-w-6xl mx-auto p-6 md:p-12">
+            <main className="flex-1 h-full overflow-y-auto overflow-x-hidden pt-16 md:pt-0 relative scroll-smooth overscroll-none">
+                <div className="max-w-6xl mx-auto p-4 md:p-8 lg:p-12 pb-24">
                     {renderContent()}
                 </div>
             </main>
