3a310d6ec80732ee60eb48972af443b9d7af95f7
Víctor Castillo <castillovictor2461@gmail.com>
2026-02-05 01:14:10 -0400

    chore: add CI, ESLint, Prettier, husky and lint-staged; enforce node engine

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'vitest'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:vitest/recommended',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    'vitest/globals': true,
+  },
+  rules: {
+    'react/prop-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+};
