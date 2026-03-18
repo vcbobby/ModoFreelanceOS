5c3d2b2d9709d9a1eb1a875f3d7b2ee82ef134b8
Víctor Castillo <castillovictor2461@gmail.com>
2026-03-17 20:05:48 -0400

    electron

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  win.loadFile('dist/index.html');
+}
+
+app.whenReady().then(createWindow);
